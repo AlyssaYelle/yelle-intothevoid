d3600c4f858d424eadc64cfff63b4e554e0d5653
Alyssa <alyssadanijones@gmail.com>
2019-11-02 14:55:26 -0400

    styled header

---
+++ y2v-frontend/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../css/header.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <span>Yelle</span> into the void
+      </h1>
+      <nav>
+        <a href="https://www.instagram.com/yelle_intothevoid/">Art</a>
+        <a href="https://www.instagram.com/yelle_intothevoid/">About</a>
+        <a href="https://www.instagram.com/yelle_intothevoid/">Code</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
