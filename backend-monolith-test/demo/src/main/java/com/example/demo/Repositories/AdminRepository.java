05c61e7863d7e50c07d3bb11ffdf7aa9f4350bb3
Alyssa <alyssadanijones@gmail.com>
2019-11-06 14:09:11 -0500

    added AdminRepository to eventually assist with login

---
+++ backend-monolith-test/demo/src/main/java/com/example/demo/Repositories/AdminRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.Repositories;
+
+import com.example.demo.Models.Admin;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends CrudRepository<Admin, Long> {
+
+    // to login i need info: username & password
+    // query will find that record in db
+    @Query("FROM Admin a WHERE a.username = ?1 and a.password = ?2")
+    public Admin login(String username, String password);
+
+}
