37d3f986dd08ec0ce3e3619b936165c55d61f2b5
Alyssa <alyssadanijones@gmail.com>
2019-11-06 12:19:48 -0500

    building a demo monolith backend to later transform into microservices

---
+++ backend-monolith-test/demo/src/main/java/com/example/demo/Models/Art.java
@@ -0,0 +1,79 @@
+package com.example.demo.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "art")
+public class Art {
+    // generate unique primary key
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // art will have a title (does not need to be unique, as several may be untitled
+    @Column
+    private String title;
+
+    // i will want to display a description
+    @Column
+    private String description;
+
+    // need to link to the art somewhere
+    @Column
+    private String link;
+
+    // going to change how i do this later, but art will be matched with a song
+    @Column
+    private String song;
+
+    /*
+    * TODO
+    *  join with song table
+    *  each song will be associated with a single piece of art
+    *  but art can be associated with multiple songs
+    * */
+
+    // getters and setters
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getSong() {
+        return song;
+    }
+
+    public void setSong(String song) {
+        this.song = song;
+    }
+}
