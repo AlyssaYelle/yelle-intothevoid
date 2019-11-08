03c4c5ee9394fa8a6e12df5f931ce7ade96254e8
Alyssa <alyssadanijones@gmail.com>
2019-11-08 11:33:05 -0500

    added custom query to art repository

--- backend-monolith-test/demo/src/main/java/com/example/demo/Repositories/ArtRepository.java
+++ backend-monolith-test/demo/src/main/java/com/example/demo/Repositories/ArtRepository.java
@@ -1,10 +1,15 @@
 package com.example.demo.Repositories;
 
 import com.example.demo.Models.Art;
+import com.example.demo.Models.ArtSongObj;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArtRepository extends CrudRepository<Art, Long> {
-    // pretty sure I don't need any custom methods
+    // select custom ArtSongObj
+    @Query("SELECT new com.example.demo.Models.ArtSongObj(a.title, a.description, a.link, s.title, s.artist, s.uri)" +
+            "FROM Songs s JOIN s.art a")
+    public Iterable<ArtSongObj> listArtWithSongs();
 }
