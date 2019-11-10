02fe23726687fd272c887ab4748980ef290a6b9b
Alyssa <alyssadanijones@gmail.com>
2019-11-10 10:41:49 -0500

    added some initial styling to modal -- need to update how props are inherited for everything

--- y2v-frontend/src/components/ArtModal.js
+++ y2v-frontend/src/components/ArtModal.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import '../css/ArtModal.css';
+import Player from './Player';
 
 
 class ArtModal extends Component {
@@ -8,8 +9,14 @@ class ArtModal extends Component {
     return (
       <div id="myModal" className="modal">
         <span className="close">×</span>
-        <img className="modal-content" id="img01" src="https://github.com/AlyssaYelle/yelle-intothevoid/raw/master/y2v-frontend/public/assets/scream.png"/>
-        <div id="caption">Test caption</div>
+        <div className="modal-content">
+          <img className="modal-img" id="img01" src="https://github.com/AlyssaYelle/yelle-intothevoid/raw/master/y2v-frontend/public/assets/scream.png" alt=""/>
+          <div id="caption">
+            <h3>today i paint because i cannot scream</h3>
+            <Player />
+            <p>you asked me why i never smile anymore so i am painting you this</p>
+          </div>
+        </div>
       </div>
     )
   }
