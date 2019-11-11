85dc144998ca4e1f556e5e9752e2b61cae3f40e0
Alyssa <alyssadanijones@gmail.com>
2019-11-11 16:19:48 -0500

    footer is good enough for now - will need to add in event handlers for dealing with form submission

--- y2v-frontend/src/components/Footer.js
+++ y2v-frontend/src/components/Footer.js
@@ -6,17 +6,28 @@ function Footer() {
     <div className='footer'>
       <footer>
         <div className='newsletter-signup'>
-          Sign up for my newsletter!
+          <form>
+            <label>
+              <p>Sign up for my newsletter!</p>
+              <input type="text" name="fname" placeholder='first name'/>
+            </label>
+            <label>
+              <input type="text" name="lname" placeholder='last name'/>
+            </label>
+            <label>
+              <input type="text" name="email" placeholder='email'/>
+            </label>
+            <input id='submit' type="submit" value="Sign up!" />
+          </form>
         </div>
         <div className='logo'>
+          <img id='logo' src='https://github.com/AlyssaYelle/yelle-intothevoid/raw/master/y2v-frontend/src/Untitled-Artwork-1.png' alt='' />
         </div>
         <div className='contact'>
-          Contact me!
+          <p>Contact me!</p>
           <ul>
             <li><a href='mailto:alyssayellle@gmail.com'>email</a></li>
-            <li><a href='https://alyssayelle.github.io/'>portfolio</a></li>
             <li><a href='https://github.com/alyssayelle'>github</a></li>
-            <li><a href='https://www.linkedin.com/in/alyssayelle/'>linkedin</a></li>
           </ul>
         </div>
       </footer>
